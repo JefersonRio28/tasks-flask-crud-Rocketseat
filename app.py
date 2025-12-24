03da8a2aafb6ad84dec95674713510f4a7130314
Jeferson Vicente <jefersonrio28@gmail.com>
2025-12-24 14:09:10 +0100

    Code task updated with creation and reading parts

--- app.py
+++ app.py
@@ -5,17 +5,44 @@ Created on Mon Dec 22 16:12:03 2025
 @author: gl-ca
 """
 
-from flask import Flask # type: ignore
+from flask import Flask, request, jsonify # type: ignore
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+#CRUD - C/Create, R/Read, U/Update, D/Delete
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks',methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data.get("title"),description=data.get("description",""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message" : "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks',methods=['GET'])
+def get_tasks():
+    task_list =  [task.to_dict() for task in tasks]
+
+    output={
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>',methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 if __name__=="__main__":
     app.run(debug=True)
